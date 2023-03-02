4eade9ba518af08eba28501a4ed6b49f9e3c66d0
akirco <earlfx@163.com>
2023-03-02 15:43:14 +0800

    refactor

--- src/controller/routeDecorator.ts
+++ src/controller/routeDecorator.ts
@@ -1,4 +1,6 @@
 import type { Context, Next } from 'koa'
+import Router from 'koa-router'
+const router = new Router()
 
 function route(path: string, method: Route.HttpMethod) {
   return function (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
@@ -10,9 +12,21 @@ function route(path: string, method: Route.HttpMethod) {
       if (ctx.path !== path) {
         return next()
       }
-      await originalMethod.call(this, ctx, next)
+
+      console.log(originalMethod.toString())
+
+      router['get'](method, path, async () => {
+        await originalMethod.call(this, ctx, next)
+      })
+      console.log(ctx.method, ctx.path)
     }
   }
 }
 
 export { route }
+
+// function Home(ctx: Context) {
+//   return __awaiter(this, void 0, void 0, function* () {
+//     ctx.body = 'hello world!'
+//   })
+// }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import Router from '@koa/router'
+import Router from 'koa-router'
 const router = new Router()
 import { Controller } from '../controller'
 
